a2ca77682c0ddbaaf566ad4a6178e62e1393c2ec
vittosba <vitsbardella@gmail.com>
2021-12-10 13:19:28 +0100

    add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,36 @@
     <header>
         <div class="container">
             <!-- nav -->
+            <nav>
+                <div class="right-nav">
+                    <img src="@/assets/avadabarbers-logo-x1.png" alt="">
+                </div>
+                <div class="left-nav">
+                    <i class="fas fa-shopping-cart"></i>
+                    <i class="fas fa-bars"></i>
+                </div>
+
+            </nav>
 
             <!-- jumbotron -->
+            <div class="jumbotron">
+                <div class="row align-items-center">
+                    <div class="col-6 left-side">
+                        <h1>Barber Shop</h1>
+                        <hr>
+                        <p>The Pinnacle of Male Grooming</p>
+                        <button class="site-btn">Learn More</button>
+                    </div>
+                    <div class="col-6 right-side">
+                        <img src="@/assets/avadabarbers_hero_focalmirror-400x550.png" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="buttons">
+                    <img src="@/assets/demos.png" alt="">
+                    <img src="@/assets/on-sale.png" alt="">
+            </div>
         </div>
     </header>
 </template>
@@ -14,6 +42,69 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
+    header {
+        position: relative;
+        background-image: url('../assets/avadabarbers-homepage-hero-bg.jpg');
+        background-size: cover;
+        background-position: center;
+        color: white;
+        nav {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding-top: 10px;
+    
+            .left-nav {
+                i {
+                    font-size: 1.2rem;
+                    margin-left: 10px;
+                    cursor: pointer;
+                }
+            }
+        }
+
+        .jumbotron {
+            padding-bottom: 70px;
+
+            .row {
+                .right-side {
+                    display: flex;
+                    justify-content: center;
+                }
+
+                .left-side {
+                    h1 {
+                        font-size: 3.5rem;
+                    }
+                    hr {
+                        width: 25%;
+                        border: 1.5px solid #ae8652;
+                        opacity: 1;
+                    }
+                    p {
+                        padding: 1rem 0;
+                        font-size: 0.9rem;
+                        color: #505050;
+                    }
+                }
+            }
+
+        }
+
+        .buttons {
+            display: flex;
+            flex-direction: column;
+            position: absolute;
+            top: 150px;
+            right: 20px;
+
+            img {
+                width: 50px;
+                margin-bottom: 10px;
+                cursor: pointer;
+            }
+        }
+    }
 </style>
